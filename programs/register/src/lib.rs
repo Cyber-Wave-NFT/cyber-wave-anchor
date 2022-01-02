7d15dce8d0b7b5dcf3ef5ae4481245d6f147b8c6
shlee <shlee@intellisys.co.kr>
2022-01-02 13:29:45 +0900

    logic testing

--- programs/register/src/lib.rs
+++ programs/register/src/lib.rs
@@ -10,7 +10,7 @@ declare_id!("7xESfHY92n9LZ5GteyQX5hsJrj8kKfeYssh69TL1w3BM");
 pub mod register {
 	use super::*;
 
-	pub fn register(ctx: Context<Register>, is_deposit: u64) -> ProgramResult {
+	pub fn register(ctx: Context<Register>, is_deposit: bool) -> ProgramResult {
 		let cpi_program = ctx.accounts.dao_program.to_account_info();
 		let mut account_data = ProgramAccountInfo {
 			level: ctx.accounts.my_account.level,
@@ -25,20 +25,23 @@ pub mod register {
 			ability_used_at: ctx.accounts.my_account.ability_used_at,
 			region: ctx.accounts.my_account.region.clone()
 		};
-		account_data.level += is_deposit as u32;
-		// if is_deposit {
-		// 	account_data.power = 1.01_f64.powf((account_data.level - 1) as f64) as u32 * 1000;
-		// 	account_data.exp_per_minute = account_data.power / 600;
-		// 	account_data.registered_at = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-		// 	account_data.region = "BASEMENT".to_string();
-		// } else {
-		// 	let time_elapsed: u64 = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs() - account_data.registered_at;
-		// 	account_data.region = "00000000".to_string();
-		// 	account_data.registered_at = 0;
-		// 	account_data.exp += ((time_elapsed as u32) / 60) * account_data.exp_per_minute;
-		// 	account_data.level = 1 + ((account_data.exp / 50) as f64).sqrt().round() as u32;
-		// 	account_data.exp_per_minute = 0;
-		// }
+		// account_data.level += is_deposit as u32;
+		if is_deposit {
+			// account_data.level += is_deposit as u32;
+			account_data.power = 1.01_f64.powf((account_data.level - 1) as f64) as u32 * 1000;
+			account_data.level = 1 + ((account_data.exp / 50) as f64).sqrt().round() as u32;
+			// account_data.exp_per_minute = account_data.power / 600;
+			// account_data.registered_at = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+			// account_data.region = "BASEMENT".to_string();
+		} else {
+			// account_data.level -= is_deposit as u32;
+			// let time_elapsed: u64 = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs() - account_data.registered_at;
+			// account_data.region = "00000000".to_string();
+			// account_data.registered_at = 0;
+			// account_data.exp += ((time_elapsed as u32) / 60) * account_data.exp_per_minute;
+			// account_data.level = 1 + ((account_data.exp / 50) as f64).sqrt().round() as u32;
+			// account_data.exp_per_minute = 0;
+		}
 		
 		let cpi_accounts = SetData {
 			account: ctx.accounts.my_account.to_account_info(),
--- tests/test-cpi.test.ts
+++ tests/test-cpi.test.ts
@@ -1,4 +1,5 @@
 import * as anchor from '@project-serum/anchor'
+import { AccountInfo } from '@solana/spl-token'
 import * as borsh from 'borsh'
 class ProgramAccountInfo {
 	level = 0
@@ -70,48 +71,51 @@ describe('cpi', () => {
 	const register = anchor.workspace.Register
 
 	// 로컬 월렛 키페어 가져오기
-	const a_key = Buffer.from([27,81,124,213,249,242,152,45,212,167,200,161,9,96,58,203,232,4,201,30,99,191,222,174,66,178,120,40,80,181,162,2,123,181,112,155,206,105,144,205,15,98,43,19,29,175,201,37,106,60,94,158,35,195,120,224,95,239,53,54,67,86,118,185])
-	const b_key = Buffer.from([182,218,165,125,105,218,250,172,86,209,102,1,218,251,206,177,250,78,56,113,20,4,22,171,78,111,161,40,244,247,244,144,57,245,94,190,210,65,28,230,82,227,71,169,143,213,13,89,123,225,138,180,163,29,68,0,88,235,40,114,106,104,234,184])
+	const aKey = Buffer.from([27,81,124,213,249,242,152,45,212,167,200,161,9,96,58,203,232,4,201,30,99,191,222,174,66,178,120,40,80,181,162,2,123,181,112,155,206,105,144,205,15,98,43,19,29,175,201,37,106,60,94,158,35,195,120,224,95,239,53,54,67,86,118,185])
+	const bKey = Buffer.from([182,218,165,125,105,218,250,172,86,209,102,1,218,251,206,177,250,78,56,113,20,4,22,171,78,111,161,40,244,247,244,144,57,245,94,190,210,65,28,230,82,227,71,169,143,213,13,89,123,225,138,180,163,29,68,0,88,235,40,114,106,104,234,184])
 
 	// 클라이언트 월렛 어카운트
-	const a_clientWalletAccount = anchor.web3.Keypair.fromSecretKey(a_key)
-	const b_clientWalletAccount = anchor.web3.Keypair.fromSecretKey(b_key)
+	const aClientWalletAccount = anchor.web3.Keypair.fromSecretKey(aKey)
+	const bClientWalletAccount = anchor.web3.Keypair.fromSecretKey(bKey)
 
-	console.log(a_clientWalletAccount.publicKey.toBase58())
-	console.log(b_clientWalletAccount.publicKey.toBase58())
+	console.log(aClientWalletAccount.publicKey.toBase58())
+	console.log(bClientWalletAccount.publicKey.toBase58())
 
-	let newDataAccount: anchor.web3.PublicKey
+	let newDataAccountPubkey: anchor.web3.PublicKey
+	// let newDataAccount: AccountInfo
 
 	const SIZE = borsh.serialize(
 		ProgramAccountInfoSchema,
 		new ProgramAccountInfo(),
 	  ).length + 8
-	it('test amu', async () => {
-		const SEED = '11111111112222222222333333333320' // spl token
+	it('Check and create Dao Data Account', async () => {
+		const SEED = 'nft_3' // spl token
 		// 클라 퍼블릭키, SPL token ID, DAO 프로그램 ID로 새 데이터 어카운트 생성 (혹은 이미 있는 어카운트 가져오기)
-		newDataAccount = await anchor.web3.PublicKey.createWithSeed(
-			b_clientWalletAccount.publicKey,
+		newDataAccountPubkey = await anchor.web3.PublicKey.createWithSeed(
+			bClientWalletAccount.publicKey,
 			SEED,
 			dao.programId
 		)
 		// newDataAccount가 가지고있는 DAO 소속 data account
-		const dataAccount = await dao.provider.connection.getAccountInfo(newDataAccount)
+		const newDataAccount = await dao.provider.connection.getAccountInfo(newDataAccountPubkey)
 		// data account가 null이면 DAO가 홀딩하는 data account를 만들어준다.
 		// null이 아니면 그냥 해당 데이터 어카운트를 사용한다.
-		if (dataAccount === null) {
-			console.log('Creating account', newDataAccount.toBase58(), 'to say hello to')
+		if (newDataAccount === null) {
+			console.log('Creating account', newDataAccountPubkey.toBase58(), 'to say hello to')
 
 			// 데이터 사이즈에 맞는 최소 rent비 무시 적재량 계산
-			const lamports = await dao.provider.connection.getMinimumBalanceForRentExemption(SIZE)
+			const lamports = await provider.connection.getMinimumBalanceForRentExemption(SIZE)
+			let prevLamports = await provider.connection.getBalance(aClientWalletAccount.publicKey)
+			console.log(prevLamports / 1000000000)
 
 			// 트랜잭션
 			let createNewAccDao = new anchor.web3.Transaction().add(
 				// create account
 				anchor.web3.SystemProgram.createAccountWithSeed({
-					fromPubkey: a_clientWalletAccount.publicKey,
-					basePubkey: b_clientWalletAccount.publicKey,
+					fromPubkey: aClientWalletAccount.publicKey,
+					basePubkey: bClientWalletAccount.publicKey,
 					seed: SEED,
-					newAccountPubkey: newDataAccount,
+					newAccountPubkey: newDataAccountPubkey,
 					lamports,
 					space: SIZE,
 					programId: dao.programId,
@@ -119,37 +123,61 @@ describe('cpi', () => {
 			)
 
 			// 트랜잭션 실제 발생
-			await dao.provider.send(createNewAccDao, [a_clientWalletAccount])
+			await dao.provider.send(createNewAccDao, [aClientWalletAccount])
 			const tx = await dao.rpc.initialize({
 				accounts: {
-					myAccount: newDataAccount,
+					myAccount: newDataAccountPubkey,
 					user: provider.wallet.publicKey,
 					systemProgram: anchor.web3.SystemProgram.programId,
 				},
 				signers: [],
 			})
+			// await register.rpc.register(false, {
+			// 	accounts: {
+			// 		myAccount: newDataAccountPubkey,
+			// 		daoProgram: dao.programId,
+			// 	},
+			// })
+			// const result = await dao.account.programAccountInfo.fetch(newDataAccountPubkey)
+			// console.log(result['level'])
+
 			console.log('Your transaction signature', tx)
-			console.log(`newDataAccount:${newDataAccount}`)
+			console.log(`newDataAccount:${newDataAccountPubkey}`)
+
+			let postLamports = await provider.connection.getBalance(bClientWalletAccount.publicKey)
+			console.log(postLamports / 1000000000)
 		} else {
 			console.log('success')
-			console.log(newDataAccount.toBase58())
+			console.log(newDataAccountPubkey.toBase58())
 		}
 	})
 
 	it('Send transaction to register program', async () => {
 		try {
-			await register.rpc.register(new anchor.BN(1), {
+			console.log("is it deposit?")
+			console.log(newDataAccountPubkey.toBase58())
+
+			const isDeposit = true
+			const dataAccount = await dao.account.programAccountInfo.fetch(newDataAccountPubkey)
+			if (isDeposit && dataAccount['registered_at'] !== 0) {
+				console.log("You are trying to register but Already registered")
+			} else if (!isDeposit && dataAccount['registered_at'] === 0) {
+				console.log("You are trying to unregister but isn't Registered.")
+			}
+
+			await register.rpc.register(isDeposit, {
 				accounts: {
-					myAccount: newDataAccount,
+					myAccount: newDataAccountPubkey,
 					daoProgram: dao.programId,
 				},
 			})
 
-			const result = await dao.account.programAccountInfo.fetch(newDataAccount)
+			const result = await dao.account.programAccountInfo.fetch(newDataAccountPubkey)
 			console.log(result['level'])
-			expect(result['level']).toBe(1)
+			// expect(result['level']).toBe(1)
 		} catch (err) {
-			throw new Error()
+			console.log(err)
+			fail(err)
 		}
 	})
 })
