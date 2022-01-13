7221038b2ea15ad2f29ca43109ad9969f9db394f
Jihwan Park <pjhh88@snu.ac.kr>
2022-01-14 01:43:23 +0900

    error fix 1

--- tests/test-wave.test.ts
+++ tests/test-wave.test.ts
@@ -1,6 +1,7 @@
 import * as anchor from '@project-serum/anchor'
 import * as borsh from 'borsh'
 import { ProgramAccountInfo, ProgramAccountInfoSchema, RegionInfo, RegionInfoSchema } from './borsh.classes'
+import {Utils} from './utils/utils'
 
 jest.setTimeout(30000000)
 describe('cpi', () => {
@@ -10,26 +11,22 @@ describe('cpi', () => {
 	anchor.setProvider(provider)
 	let newDataAccountPubkey: anchor.web3.PublicKey
 	// DAO 프로그램, register 프로그램 가져오기
-	const register = anchor.workspace.Register
-	const waveSizeCalculation = anchor.workspace.WaveSizeCalculation
+	const cyberWave = anchor.workspace.CyberWave
 
 	// 로컬 월렛 키페어 가져오기
 	const serverMainKey = Buffer.from([27,81,124,213,249,242,152,45,212,167,200,161,9,96,58,203,232,4,201,30,99,191,222,174,66,178,120,40,80,181,162,2,123,181,112,155,206,105,144,205,15,98,43,19,29,175,201,37,106,60,94,158,35,195,120,224,95,239,53,54,67,86,118,185])
-	const clientKey = Buffer.from([6,47,179,252,50,159,110,185,226,7,125,211,8,220,207,220,93,30,98,221,88,235,0,62,38,247,67,47,171,86,243,228,234,167,128,167,139,174,181,164,94,129,41,59,157,64,164,48,61,60,26,133,57,176,25,73,112,74,136,40,112,115,174,238])
 
 	// 클라이언트 월렛 어카운트
-	const clientWalletAccount = anchor.web3.Keypair.fromSecretKey(clientKey)
 	const serverWalletAccount = anchor.web3.Keypair.fromSecretKey(serverMainKey)
 	
-	console.log(clientWalletAccount.publicKey.toBase58())
 	console.log(serverWalletAccount.publicKey.toBase58())
 
 	it('test rns', async () => {
-		const ts = await register.account.programAccountInfo.all()
+		const ts = await cyberWave.account.programAccountInfo.all()
 		const accounts = ts.map((elem: {publicKey: any, account: Object}) => (elem.account))
 		console.log(accounts)
 		const res = accounts.reduce((acc: any, cur: any) => {
-			acc += cur.power
+			acc += cur.levelPower * cur.powerMagnified / 10000
 			return acc
 		}, 0)
 		const SIZE = borsh.serialize(
@@ -39,20 +36,20 @@ describe('cpi', () => {
 		newDataAccountPubkey = await anchor.web3.PublicKey.createWithSeed(
 			serverWalletAccount.publicKey,
 			"CENTRAL_REGION1",
-			waveSizeCalculation.programId
+			cyberWave.programId
 		)
-		const newDataAccount = await register.provider.connection.getAccountInfo(newDataAccountPubkey)
+		const newDataAccount = await cyberWave.provider.connection.getAccountInfo(newDataAccountPubkey)
 		if (!newDataAccount) {
 			const lamports = await provider.connection.getMinimumBalanceForRentExemption(SIZE)
 			let prevLamports = await provider.connection.getBalance(serverWalletAccount.publicKey)
 			console.log(prevLamports / 1000000000)
 
 			// 트랜잭션
-			const tx = await waveSizeCalculation.rpc.initialize(
+			const tx = await cyberWave.rpc.initializeRegionData(
 				{
 				accounts: {
 					myAccount: newDataAccountPubkey,
-					user: provider.wallet.publicKey,
+					user: serverWalletAccount.publicKey,
 					systemProgram: anchor.web3.SystemProgram.programId,
 				},
 				instructions: [
@@ -63,25 +60,25 @@ describe('cpi', () => {
 						newAccountPubkey: newDataAccountPubkey,
 						lamports,
 						space: SIZE,
-						programId: waveSizeCalculation.programId,
+						programId: cyberWave.programId,
 					}),
 				],
 				signers: [serverWalletAccount],
 			})
 		}
-		await waveSizeCalculation.rpc.sizeCalculate(
+		await cyberWave.rpc.sizeCalculate(
 			new anchor.BN(res),
-			new anchor.BN(makeId(8)),
-			new anchor.BN(makeId(8)),
-			new anchor.BN(makeId(8)),
-			new anchor.BN(makeId(8)),
+			Utils.makeId(8),
+			Utils.makeId(8),
+			Utils.makeId(8),
+			Utils.makeId(8),
 			{
 			accounts: {
 				centralRegionAccount: newDataAccountPubkey,
 			},
 			signers: [],
 		})
-		const result = await waveSizeCalculation.account.regionInfo.fetch(newDataAccountPubkey)
+		const result = await cyberWave.account.regionInfo.fetch(newDataAccountPubkey)
 		console.log(result)
 	})
 
